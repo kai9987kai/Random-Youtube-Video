10702242b8957c5f413ca321e62e12b5b2fecc91
Kai Piper <kai9987kai@gmail.com>
2019-03-13 21:01:30 +0000

    Update RandomYouTubeVideos.py

--- RandomYouTubeVideos.py
+++ RandomYouTubeVideos.py
@@ -12,7 +12,7 @@ client = gspread.authorize(creds)
 sheet = client.open("likes").sheet1  # Open the spreadhseet
 data = sheet.get_all_records()  # Get a list of all records
 col = sheet.col_values(3)
-random1 = random.randint(1, 24)
+random1 = random.randint(1, 43)
 cell = sheet.cell(random1, 3).value
 
 
